bd05851cc7f7f2425024dac921a7af3c4108c41c
nirmalprajapati7790 <167669592+nirmalprajapati7790@users.noreply.github.com>
2024-04-27 06:46:52 +0000

    container config done

--- 01basics/test.js
+++ 01basics/test.js
@@ -1,1 +1,50 @@
-console.log("nirmal ji")
+// console.log("nirmal ji")
+
+// Assingnment operater
+// var x = 5
+// var y = 5
+
+
+// console.log( 'both the x and y equal or not ' +  x==y);
+// console.log( ' is both the x and y equal : ${x == y}')
+
+// arithmetic operator
+
+// console.log(3+3);
+// console.log(9-4);
+// console.log(2*3);
+// console.log(4/2);
+// console.log('remender operater ' + 27%4);
+
+// increment and decrement
+// postfix
+
+// var num = 15
+// var newNum = num++
+
+// console.log(num);
+// console.log(newNum);
+
+// prefix
+// var num = 15
+// var newNum = ++num
+
+// console.log(num);
+// console.log(newNum);
+
+// decrement operater
+
+// postfix
+
+// var num = 15
+// var newNum = num--
+
+// console.log(num);
+// console.log(newNum);
+
+// prefix
+// var num = 15
+// var newNum = --num
+
+// console.log(num);
+// console.log(newNum);
