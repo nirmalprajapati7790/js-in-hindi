19e4d3f19dd886da232e777437bb087df443c6f5
nirmalprajapati7790 <167669592+nirmalprajapati7790@users.noreply.github.com>
2024-04-28 05:37:13 +0000

    container config done

--- 01basics/test.js
+++ 01basics/test.js
@@ -48,3 +48,79 @@
 
 // console.log(num);
 // console.log(newNum);
+
+// comparison operater
+
+// var x = 15
+// var y = 12
+
+// console.log(x==y);
+// console.log(x != y);
+// console.log(x>y);
+// console.log(x<y);
+// console.log(x=>y);
+// console.log(x=>y);
+
+// logical operator
+
+//  And operater &&
+// var x = 20
+// var y = -10
+
+// console.log(x>y &&  x>=y);
+
+// or operater ||
+// var x = 20
+// var y = -10
+
+// console.log( x>y || x>y || x<y );
+
+// not operater !
+// var a = 25
+// var b = 30
+
+// console.log(!(a>b));
+
+// string operater
+
+// var myName = 'nirmal '
+
+// console.log(myName +'prajapat');
+// console.log(myName +'kumhar');
+// console.log(myName +'machiwal');
+
+
+//   4 challenge time
+
+// Sol. 1
+// console.log(3**3);
+
+// sol. 2
+
+// console.log(10 +'machiwal');
+
+// sol. 3
+// var a = 4
+// var b = 5
+
+// // output a=5 b=4 swap to number
+
+// var c = b
+// b = a
+// a = c
+
+// console.log('the value of a is '+ a);
+// console.log('the value of b is '+ b);
+
+// sol. 4
+var a = 4
+var b = 5
+
+// output a=5 b=4 swap to number
+
+a = a + b    
+b = a - b    
+a = a - b
+
+console.log('the value of a is '+ a);
+console.log('the value of b is '+ b);
